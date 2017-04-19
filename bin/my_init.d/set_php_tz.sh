b316a3bb7f80124f0eabd593df3652e9ddde7502
Rob Ellison <robe@Robs-MBP.home>
2017-04-19 12:19:07 +0100

    switch to php7

--- bin/my_init.d/set_php_tz.sh
+++ bin/my_init.d/set_php_tz.sh
@@ -7,10 +7,10 @@ if [[ $CUSTOM_PHP_INI != 'true' ]] ; then
     if [ -f /etc/container_environment/TZ ] ; then
       sed -i \
           "s#\;date\.timezone\ \=#date\.timezone\ \=\ $TZ#g" \
-          /etc/php5/cli/php.ini
+          /etc/php7/cli/php.ini
       sed -i \
           "s#\;date\.timezone\ \=#date\.timezone\ \=\ $TZ#g" \
-          /etc/php5/apache2/php.ini
+          /etc/php7/apache2/php.ini
     else
       echo "Timezone not specified by environment variable"
     #  echo UTC > /etc/container_environment/TZ
