a49767bfe848ab29513a4a231131f494fd82cfed
Rob Ellison <robe@Robs-MBP.home>
2017-04-21 10:47:46 +0100

    updating download script

--- download.sh
+++ download.sh
@@ -10,14 +10,9 @@ cd /tmp &&
 
 wget https://github.com/librenms/librenms/archive/$librenms_RELEASE.tar.gz &&
 tar xvf $librenms_RELEASE.tar.gz &&
-mv librenms-$librenms_RELEASE librenms
 rm $librenms_RELEASE.tar.gz
+mv librenms-$librenms_RELEASE librenms
 
-# I know this seems ridiculous, but since /opt/librenms/html is an external
-# volume mount, svn throws a fit about it conflicting with the tree. Pulling
-# SVN to temp directory and copying contents into /opt/librenms was just the
-# first way thought of to avoid dealing with the svn conflict resolution from
-# script.
 cp -r /tmp/librenms/* /opt/librenms/
 
 # Web interface directories
@@ -28,7 +23,7 @@ chmod 775 rrd
 
 # setup logrotate
 
-cp /opt/librenms/misc/librenms.logrotate /etc/logrotate.d/librenms
+cp /tmp/librenms/misc/librenms.logrotate /etc/logrotate.d/librenms
 
 
 # configure librenms package
